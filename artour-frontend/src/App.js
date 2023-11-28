8c219dd9f782e2ac9b776c5c11b5e576d24e60da
spencerkinney <spencer_kinney@yahoo.com>
2023-11-27 22:04:13 -0600

    added example images

--- artour-frontend/src/App.js
+++ artour-frontend/src/App.js
@@ -1,14 +1,57 @@
-import React, {useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { SceneTemplate } from "./components/SceneTemplate";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SceneTemplate from './components/SceneTemplate';
 
 function App() {
+  // Example scene data
+  const scenes = [
+    {
+      id: 1,
+      imagePath: '/img/image-20231127-195426.png',
+      title: 'Scene 1: Leaving Town',
+      decisionLabel: 'Which way would you like to go?',
+      choices: [
+        { id: 2, label: 'North' },
+        { id: 3, label: 'South' },
+        { id: 4, label: 'East' },
+        { id: 5, label: 'West' }
+      ]
+    },
+  ];
+
+  const [currentSceneId, setCurrentSceneId] = useState(1); // Starting scene
+
+  const currentScene = scenes.find(scene => scene.id === currentSceneId);
+
+  // Function to navigate to the next scene
+  const handleNextScene = (nextSceneId) => {
+    setCurrentSceneId(nextSceneId);
+  };
+
+  // Function to go back to the previous scene (optional, depends on your game logic)
+  const handlePreviousScene = () => {
+    // Implement previous scene logic
+  };
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SceneTemplate />} />
+        <Route 
+          path="/" 
+          element={
+            <SceneTemplate 
+              imagePath={currentScene.imagePath}
+              sceneTitle={currentScene.title}
+              sceneDecisionLabel={currentScene.decisionLabel}
+              decisionChoices={currentScene.choices}
+              onNextScene={handleNextScene}
+              onPreviousScene={handlePreviousScene}
+            />
+          } 
+        />
       </Routes>
     </Router>
   );
 }
+
 export default App;
--- artour-frontend/src/components/SceneTemplate.js
+++ artour-frontend/src/components/SceneTemplate.js
@@ -1,9 +1,59 @@
-function SceneTemplate() {
-    return (
-        <div>
-        <h1>SceneTemplate</h1>
-        </div>
-    );
-}
+import React from "react";
+import {
+  Box,
+  Button,
+  VStack,
+  Image,
+  Text,
+  Heading,
+  SimpleGrid,
+  Container,
+} from "@chakra-ui/react";
+
+const SceneTemplate = ({
+  imagePath,
+  sceneTitle,
+  sceneDecisionLabel,
+  decisionChoices,
+  onNextScene,
+  onPreviousScene,
+}) => {
+  return (
+    <Container maxW="container.lg" p={0}>
+      <VStack spacing={6} align="stretch"> {/* Increased spacing */}
+        {/* Image with a subtle overlay */}
+        <Box position="relative" h={{ base: "40vh", sm: "50vh", md: "60vh", lg: "75vh" }}>
+          <Image src={imagePath} w="full" h="full" objectFit="cover" />
+          <Box position="absolute" top="0" left="0" w="full" h="full" bg="blackAlpha.300" />
+        </Box>
+
+        {/* Centered Content Container */}
+        <Box px={12} w="full" display="flex" flexDirection="column" alignItems="center">
+          {/* Scene Title and Description */}
+          <Heading as="h2" size="md" textAlign="center" fontWeight="normal">
+            {sceneTitle}
+          </Heading>
+          <Text mt={3} textAlign="center" color="gray.500">
+            {sceneDecisionLabel}
+          </Text>
+
+          {/* Decision Choices */}
+          <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5} pt={5} w="full">
+            {decisionChoices.map((choice, index) => (
+              <Button
+                key={index}
+                onClick={() => onNextScene(choice.id)}
+                colorScheme="gray"  // Subdued color scheme
+                variant="outline"  // Minimalistic outline buttons
+              >
+                {choice.label}
+              </Button>
+            ))}
+          </SimpleGrid>
+        </Box>
+      </VStack>
+    </Container>
+  );
+};
 
 export default SceneTemplate;
