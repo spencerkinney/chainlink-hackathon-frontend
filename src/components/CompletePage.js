04cbc67a7d6cb335c97ce48c592a472eb5161e01
spencerkinney <spencer_kinney@yahoo.com>
2023-12-01 17:28:45 -0600

    complete page

--- src/components/CompletePage.js
+++ src/components/CompletePage.js
@@ -1,12 +1,27 @@
 import { Box, Heading, Text, CheckIcon, Center, VStack, CheckboxIcon, Progress } from "@chakra-ui/react";
 
 function CompletePage({ progress }) {
+
+    let message;
+    if (progress <= 20) {
+        message = "Congrats! You've started your journey.";
+    } else if (progress <= 40) {
+        message = "Congrats! You're making great progress.";
+    } else if (progress <= 60) {
+        message = "Congrats! You're more than halfway through your journey.";
+    } else if (progress <= 80) {
+        message = "Congrats! Almost completed your journey.";
+    } else {
+        message = "Completed! Time to mint your NFT!";
+    }
+
+    
     return (
         <Center h="100vh">
             <VStack spacing={4}>
-                <CheckboxIcon boxSize={12} color="green.500" />
+                <CheckboxIcon boxSize={12} color="green.300" />
                 <Heading>Scene Completed</Heading>
-                <Text fontSize="xl">Congrats! You've successfully completed part of your journey.</Text>
+                <Text fontSize="xl">{message}</Text>
                 <Progress value={progress} hasStripe width="80%" colorScheme="green" size="lg" />
             </VStack>
         </Center>
