78858e3ffd9d03e739e86d66eae11b9799b98b9b
spencerkinney <spencer_kinney@yahoo.com>
2023-12-02 13:33:51 -0600

    updated characterr page

--- src/components/CharacterPage.js
+++ src/components/CharacterPage.js
@@ -7,54 +7,49 @@ import {
 const CharacterPage = ({ onGameStart }) => {
   const bg = useColorModeValue('gray.50', 'gray.900');
   const textColor = useColorModeValue('gray.600', 'gray.200');
-  const buttonColor = useColorModeValue('whatsapp.500', 'whatsapp.300');
 
   return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
-      height="100vh"
+    <VStack
+      spacing={6}
+      align="center"
+      justify="center"
+      minH="100vh"
       bg={bg}
       color={textColor}
-      padding={4}
+      px={4}
+      width="full"
     >
-      <VStack spacing={8} maxWidth="sm" width="full" px={4}>
-        <Text fontSize="2xl" fontWeight="normal" textAlign="center">
-          Alarmin The Outcast
+      <Text fontSize="2xl" fontWeight="normal" textAlign="center">
+        Alarmin The Outcast
+      </Text>
+      <Box
+        borderRadius="md"
+        boxSize={['250px', '300px']} // Responsive image size
+        boxShadow="md"
+        overflow="hidden"
+      >
+        <Image
+          src="/img/char/1.png" // Update with actual image path
+          alt="Character Image"
+          objectFit="cover"
+          width="100%"
+          height="auto"
+        />
+      </Box>
+      <Container centerContent p={2}>
+        <Text fontSize="md" fontWeight="normal" textAlign="center">
+          A Drow sorcerer shrouded in mystery, wielding the arcane with an outcast's precision.
         </Text>
-        <Box
-          borderRadius="md"
-          boxSize="300px"
-          boxShadow="md"
-          overflow="hidden"
-        >
-          <Image
-            src="/img/char/1.png" // Update with actual image path
-            alt="Character Image"
-            objectFit="cover"
-            width="100%"
-            height="auto"
-          />
-        </Box>
-        <Container centerContent>
-          <Text fontSize="md" fontWeight="normal" textAlign="center">
-            A Drow sorcerer shrouded in mystery, wielding the arcane with an outcast's precision.
-          </Text>
-        </Container>
-        <Button
-          size="md"
-          bg={buttonColor}
-          color="white"
-          _hover={{ bg: 'whatsapp.600' }}
-          width="full"
-          onClick={onGameStart}
-        >
-          START
-        </Button>
-      </VStack>
-    </Box>
+      </Container>
+      <Button
+        size="md"
+        colorScheme='whatsapp'
+        px={8}
+        onClick={onGameStart}
+      >
+        START
+      </Button>
+    </VStack>
   );
 };
 
