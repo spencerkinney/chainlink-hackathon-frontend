df5bc8288e641f81ae8f6ccb75cb207088ccd194
spencerkinney <spencer_kinney@yahoo.com>
2023-12-02 12:03:59 -0600

    lazy loading proper

--- src/components/GameMasterPage.js
+++ src/components/GameMasterPage.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import { Box, Heading, Image, Button, Flex, Text } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { Box, Heading, Image, Button, Flex, Text, Skeleton } from '@chakra-ui/react';
 import { ChevronRightIcon } from '@chakra-ui/icons';
 
 const GameMasterPage = ({ qrCodes }) => {
-  const [currentQRIndex, setCurrentQRIndex] = React.useState(0);
+  const [currentQRIndex, setCurrentQRIndex] = useState(0);
+  const [isImageLoading, setIsImageLoading] = useState(true);
 
   const handleNextScene = () => {
     setCurrentQRIndex((prevIndex) => (prevIndex + 1) % qrCodes.length);
+    setIsImageLoading(true); // Reset loading state for new image
   };
 
   return (
@@ -27,13 +29,17 @@ const GameMasterPage = ({ qrCodes }) => {
         p={5} 
         mb={6} 
         bg="gray.700"
+        position="relative"
       >
         <Heading size="md" mb={4}>{`Scene ${currentQRIndex + 1}`}</Heading>
+        {isImageLoading && <Skeleton height="250px" width="250px"/>}
         <Image 
           src={qrCodes[currentQRIndex]} 
           alt={`QR Code for Scene ${currentQRIndex + 1}`} 
           maxW="250px"
-          lazyLoad
+          loading="lazy"  // Native lazy loading
+          onLoad={() => setIsImageLoading(false)}
+          display={isImageLoading ? 'none' : 'block'}
         />
       </Box>
       <Button 
